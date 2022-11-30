a5fd9689700e8efbbcb31add749bca7819ce982c
SALVADOR MONTOYA <sm70996@usc.edu>
2022-11-30 07:32:20 +0000

    Finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,5 +1,3 @@
-
-
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -19,7 +17,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool isValid(int row, int col, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched); 
+bool schedHelper(int row, int col, AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched); 
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,8 +35,97 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    int numRows = avail.size(); 
+    int numCols = dailyNeed; 
 
+    //initialize array to schedule workers 
+    std::vector<Worker_T> schedRow; 
+    for(int i = 0; i < numRows; i++){
+        for(int j = 0; j < numCols; j++){
+            schedRow.push_back(INVALID_ID); 
+        }
+        //add vector to schedule 
+        sched.push_back(schedRow); 
+        schedRow.clear(); 
+    }
 
+    AvailabilityMatrix available = avail; 
 
+    //call helper function 
+    if(schedHelper(0, 0, available, dailyNeed, maxShifts, sched)){
+        return true; 
+    }
+    else{
+        return false; 
+    }
+
+}
+
+bool isValid(int row, int col, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched)
+{
+    int need = dailyNeed; 
+    int days = sched.size(); 
+
+    //get worker ID thats in the schedule 
+    Worker_T worker = sched[row][col]; 
+
+    size_t count = 0; 
+
+   //iterate through schedule to ensure you don't pass maxShifts
+    for(int i = 0; i < days; i++){
+        for(int j = 0; j < need; j++){
+            if(sched[i][j] == worker){
+                count++; 
+            }
+        }
+    }
+    if(count > maxShifts){
+        return false; 
+    }
+    else{
+        return true; 
+    }
+}
+
+bool schedHelper(int row, int col, AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched)
+{
+    int need = dailyNeed; 
+    int days = avail.size(); 
+
+    //get number of all workers 
+    Worker_T workers = avail[0].size(); 
+    Worker_T schedWorker; 
+
+    if(sched[row][col] == INVALID_ID){
+        for(Worker_T i = 0; i < workers; i++){
+            //worker is available that day
+            if(avail[row][i] == 1){
+                sched[row][col] = i; 
+                avail[row][i] = 0; 
+                schedWorker = i; 
+                //check if current schedule is valid
+                if(isValid(row,col,dailyNeed,maxShifts,sched)){
+                    //check if next schedule is valid
+                    if(col < need - 1){
+                        if(schedHelper(row, col + 1, avail, dailyNeed, maxShifts, sched)){
+                            return true; 
+                        }
+                    }
+                    else if(row < days - 1){
+                        if(schedHelper(row + 1, 0, avail, dailyNeed, maxShifts, sched)){
+                            return true; 
+                        }
+                    }
+                    else{
+                        return true; 
+                    }
+                }
+            }
+            //reset values for backtracking 
+            avail[row][schedWorker] = 1; 
+            sched[row][col] = INVALID_ID; 
+        }
+    }
+    return false; 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelp(std::string in, const std::string floating, const std::set<std::string>&dict, std::set<std::string>& possibleWords, size_t index); 
 
 
 // Definition of primary wordle function
@@ -19,8 +20,66 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-    
+		
+	//set to hold possible answers
+    std::set<std::string> possibleWords;
+	size_t index = 0; 
+    //recursive call to helper function 
+	wordleHelp(in, floating, dict, possibleWords, index); 
+	return possibleWords; 
 
 }
 
 // Define any helper functions here
+/********************************************************
+****Recursively call helper function to insert ***********
+****letters into blank spaces to see ****************
+****if you get a valid word  ******************
+**************************************************/
+void wordleHelp(std::string in, const std::string floating, const std::set<std::string>& dict, std::set<std::string>& possibleWords, size_t index)
+{
+    //checks to see if completed word is valid 
+    if(in.size() == index){
+        if(dict.find(in) != dict.end()){
+            possibleWords.insert(in); 
+        }
+        return; 
+    }
+    else if(in[index] != '-'){
+        wordleHelp(in, floating, dict, possibleWords, index + 1);
+    }
+    else{
+        //recursively calls function by inserting misplaced letters into blanks
+        std::set<char> floats; 
+        std::string alphabet = "abcdefghijklmnopqrstuvwxyz"; 
+        for(size_t i = 0; i < floating.size(); i++){
+            floats.insert(floating[i]); 
+            in[index] = floating[i]; 
+            std::string newFloat = floating.substr(0, i) + floating.substr(i+1, floating.size()); 
+            wordleHelp(in, newFloat, dict, possibleWords, index + 1);
+            in[index] = '-';  
+        }
+
+        //fills rest of blanks with random letters from alphabet 
+        bool space = true; 
+        size_t numSpaces = 0; 
+        for(size_t j = 0; j < in.size(); j++){
+            if(in[j] == '-'){
+                numSpaces++; 
+            }
+        }
+        if(numSpaces != floating.size() ){
+            space = false; 
+        }
+        if(!space){
+            for(size_t k = 0; k < alphabet.size(); k++){
+                if(floats.find(alphabet[k]) == floats.end()){
+                    in[index] = alphabet[k]; 
+                    wordleHelp(in, floating, dict, possibleWords, index + 1); 
+                    in[index] = '-' ; 
+                }
+            }
+        }
+    }
+
+}
